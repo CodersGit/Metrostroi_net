fc2fa565099db3465007490114d8241e4e999faa
Sergey Zhirov <sergey@worldsofcubes.net>
2015-12-09 20:50:42 +0600

    Рабочий список по /players

--- pages/players.php
+++ pages/players.php
@@ -1,6 +1,6 @@
 <?php
 $amount_by_page = 25;
-$page = (isset($lnk[1]))? 1: (int) $lnk[1];
+$page = (!isset($lnk[1]))? 1: (int) $lnk[1];
 $first = ($page - 1) * $amount_by_page;
 $query = $db->execute("SELECT * FROM `groups`");
 while ($gr = $db->fetch_array($query))
@@ -8,9 +8,9 @@ while ($gr = $db->fetch_array($query))
 $query = $db->execute("SELECT * FROM `players` LIMIT $first, $amount_by_page");
 Mitrastroi::TakeTPL("players/players_head");
 for($c = $first + 1; $typical_ple = $db->fetch_array($query); $c++) {
-//	include Mitrastroi::PathTPL("players/player_row");
+	include Mitrastroi::PathTPL("players/player_row");
 }
 Mitrastroi::TakeTPL("players/players_foot");
 $query = $db->execute("SELECT COUNT(*) FROM `players`");
 $query = $db->fetch_array($query);
-echo Mitrastroi::GeneratePagination($page, $amount_by_page, $query[0], "players/");
+echo Mitrastroi::GeneratePagination($page, $amount_by_page, $query[0], "/players/");
