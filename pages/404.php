68126bf61ee4dcdfdf30b09b0e10559a0fec1b68
Sergey Zhirov <sergey@worldsofcubes.net>
2015-12-20 18:43:42 +0600

    404 Not Found

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,10 @@
-Yeah!Page not found!
+<?php
+$amount_by_page = 25;
+header("HTTP/1.0 404 Not Found");
+$page_fucking_title = "Страница не найдена";
+$menu->set_item_active('players');
+include Mitrastroi::PathTPL("header");
+
+include Mitrastroi::PathTPL("404");
+
+include Mitrastroi::PathTPL("footer");
