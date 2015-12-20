0c898995b088998da10cbad9e66398eff41c1841
Sergey Zhirov <sergey@worldsofcubes.net>
2015-12-20 20:32:31 +0600

    Гайд

---
+++ pages/guide.php
@@ -0,0 +1,10 @@
+<?php
+$page_fucking_title = "Добавить пользователя...";
+$menu->set_item_active('guide');
+include Mitrastroi::PathTPL("header");
+include Mitrastroi::PathTPL("guide/left_side");
+
+include Mitrastroi::PathTPL("guide/guide");
+
+include Mitrastroi::PathTPL("guide/right_side");
+include Mitrastroi::PathTPL("footer");
