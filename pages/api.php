297fae851d7291d8c13d9ebc1c2e5d10c3416fc3
Sergey Zhirov <sergey@worldsofcubes.net>
2016-01-14 22:00:35 +0600

    Вся инфа о юзере

--- pages/api.php
+++ pages/api.php
@@ -16,6 +16,49 @@ switch ($lnk[1]) {
 			exit($query['reason']);
 		}
 		break;
+	case 'user':
+		if (!isset($lnk[2])) {
+			include MITRASTROI_ROOT . "pages/404.php";
+			exit;
+		}
+		$pl = new User($lnk[2], 'SID');
+		if ($pl->uid() < 1) {
+			exit;
+		}
+		$pl_warns = $db->execute("SELECT * FROM `violations` LEFT JOIN `user_info_cache` ON `violations`.`admin`=`user_info_cache`.`steamid` WHERE  `SID`='{$pl->steamid()}'") or die ($db->error());
+		$pl_exams = $db->execute("SELECT * FROM `groups`, `examinfo` LEFT JOIN `user_info_cache` ON `examinfo`.`examiner`=`user_info_cache`.`steamid` WHERE `examinfo`.`rank`=`groups`.`txtid` AND `SID`='{$pl->steamid()}'") or die ($db->error());
+		$pl_warns_array = array();
+		while ($pl_warn = $db->fetch_array($pl_warns)) {
+			$pl_warn_array = array(
+				'date' => $pl_warn['date'],
+				'admin' => $pl_warn['admin'],
+				'server' => $pl_warn['server'],
+				'violation' => $pl_warn['violation']
+			);
+			array_push($pl_warns_array, $pl_warn_array);
+		}
+		$pl_exams_array = array();
+		while ($pl_exam = $db->fetch_array($pl_exams)) {
+			$pl_exam_array = array(
+				'date' => $pl_exam['date'],
+				'examiner' => $pl_exam['examiner'],
+				'rank' => $pl_exam['rank'],
+				'server' => $pl_exam['server'],
+				'type' => $pl_exam['type'],
+				'note' => $pl_exam['note']
+			);
+			array_push($pl_exams_array, $pl_exam_array);
+		}
+		$pl_array = array(
+			'group' => $pl->take_group_info('txtid'),
+			'coupon' => $pl->take_coupon_info('nom'),
+			'coupon_admin' => $pl->take_coupon_info('admin'),
+			'coupon_date' => $pl->take_coupon_info('date'),
+			'violations' => $pl_warns_array,
+			'exams' => $pl_exams_array,
+		);
+		exit(json_encode($pl_array));
+		break;
 	default:
 		include MITRASTROI_ROOT . "pages/404.php";
 		exit;
