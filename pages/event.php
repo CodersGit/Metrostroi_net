ff21ca1dabb897776e1dc97ccff486a4a5699971
Sergey Zhirov <sergey@worldsofcubes.net>
2016-06-10 23:33:47 +0600

    Олди сука мразь рукожопная чтоб его бляха муха ебали в аду горящие пидоры с тату в виде логотипа Adobe Muse

--- pages/event.php
+++ pages/event.php
@@ -11,6 +11,12 @@ switch ($lnk[1]) {
 	case 'day1':
 		include Mitrastroi::PathTPL("event_dayOne");
 		break;
+	case 'day2':
+		include Mitrastroi::PathTPL("event_dayTwo");
+		break;
+	case 'day3':
+		include Mitrastroi::PathTPL("event_dayThree");
+		break;
 	default:
 		include MITRASTROI_ROOT . "pages/404.php";
 		break;
