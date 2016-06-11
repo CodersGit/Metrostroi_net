ea46a4a7554c924092c08ac28b124e38af612f3d
Sergey Zhirov <sergey@worldsofcubes.net>
2016-06-11 21:12:14 +0700

    no message

--- pages/event.php
+++ pages/event.php
@@ -4,6 +4,7 @@ if (!isset($lnk[1])) {
 	include Mitrastroi::PathTPL("event_index");
 	exit();
 }
+$day = 1;
 switch ($lnk[1]) {
 	case '':
 		include Mitrastroi::PathTPL("event_index");
@@ -12,10 +13,10 @@ switch ($lnk[1]) {
 		include Mitrastroi::PathTPL("event_dayOne");
 		break;
 	case 'day2':
-		include Mitrastroi::PathTPL("event_dayTwo");
+		include ($day >= 2)? Mitrastroi::PathTPL("event_dayTwo"): (MITRASTROI_ROOT . "pages/404.php");
 		break;
 	case 'day3':
-		include Mitrastroi::PathTPL("event_dayThree");
+		include ($day >= 3)? Mitrastroi::PathTPL("event_dayThree"): (MITRASTROI_ROOT . "pages/404.php");
 		break;
 	default:
 		include MITRASTROI_ROOT . "pages/404.php";
