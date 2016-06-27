b8ae1927183a0940ff1ecab4291c4f2e88ada46a
Sergey Zhirov <sergey@worldsofcubes.net>
2016-06-27 23:47:58 +0600

    Revert "no message"
    This reverts commit ea46a4a7554c924092c08ac28b124e38af612f3d.

--- pages/event.php
+++ pages/event.php
@@ -4,7 +4,6 @@ if (!isset($lnk[1])) {
 	include Mitrastroi::PathTPL("event_index");
 	exit();
 }
-$day = 1;
 switch ($lnk[1]) {
 	case '':
 		include Mitrastroi::PathTPL("event_index");
@@ -13,10 +12,10 @@ switch ($lnk[1]) {
 		include Mitrastroi::PathTPL("event_dayOne");
 		break;
 	case 'day2':
-		include ($day >= 2)? Mitrastroi::PathTPL("event_dayTwo"): (MITRASTROI_ROOT . "pages/404.php");
+		include Mitrastroi::PathTPL("event_dayTwo");
 		break;
 	case 'day3':
-		include ($day >= 3)? Mitrastroi::PathTPL("event_dayThree"): (MITRASTROI_ROOT . "pages/404.php");
+		include Mitrastroi::PathTPL("event_dayThree");
 		break;
 	default:
 		include MITRASTROI_ROOT . "pages/404.php";
